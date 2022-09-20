2553c3a08f38bcc8092faaa9509ed71568c2f7e6
seolranlee <leesugar90@gmail.com>
2022-09-20 18:11:29 +0900

    이벤트 처리: (1) 불편함을 감지하기

--- src/components/Items.js
+++ src/components/Items.js
@@ -8,15 +8,27 @@ export default class Items extends Component {
 		const { items } = this.$state;
 		return `
 			<ul>
-				${items.map(item => `<li>${item}</li>`).join('')}
+				${items.map((item, key) => `<li>${item} <button class="deleteBtn" data-index="${key}">삭제</button></li>`).join('')}
 			</ul>
-			<button>추가</button>
+			<button class="addBtn">추가</button>
 		`;
 	}
+	// 불편함 1. render를 실행할 때 마다 이벤트가 새로 등록된다
 	setEvent() {
-		this.$target.querySelector('button').addEventListener('click', () => {
+		// 추가
+		this.$target.querySelector('.addBtn').addEventListener('click', () => {
 			const { items } = this.$state;
 			this.setState({ items: [...items, `item${items.length + 1}`] });
 		});
+
+		// 삭제
+		// 불편함 2. 반복적인 요소에 대해 각각 이벤트를 등록해야 한다
+		this.$target.querySelectorAll('.deleteBtn').forEach(deleteBtn =>
+			deleteBtn.addEventListener('click', ({ target }) => {
+				const items = [...this.$state.items];
+				items.splice(target.dataset.index, 1);
+				this.setState({ items });
+			})
+		);
 	}
 }
